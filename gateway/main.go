d82def676a8f86727a4720438e425f6650992b91
Vladimir Zoric <vladimir.zorke@gmail.com>
2026-05-07 13:51:14 +0200

    chore: remove example service

--- gateway/main.go
+++ gateway/main.go
@@ -13,7 +13,6 @@ import (
 
 	authv1 "github.com/pyroaktiv/soa-tourism/gateway/gen/go/tourism/auth/v1"
 	blogv1 "github.com/pyroaktiv/soa-tourism/gateway/gen/go/tourism/blog/v1"
-	examplev1 "github.com/pyroaktiv/soa-tourism/gateway/gen/go/tourism/example/v1"
 	followerv1 "github.com/pyroaktiv/soa-tourism/gateway/gen/go/tourism/follower/v1"
 	stakeholdersv1 "github.com/pyroaktiv/soa-tourism/gateway/gen/go/tourism/stakeholders/v1"
 	"github.com/pyroaktiv/soa-tourism/gateway/internal/config"
@@ -40,23 +39,17 @@ func main() {
 	// 2. run `make generate`.
 	// 4. call RegisterNewServiceHandlerFromEndpoint here.
 
-	if err := examplev1.RegisterHealthServiceHandlerFromEndpoint(
-		ctx, grpcMux, cfg.ServiceAddr("example"), dialOpts,
-	); err != nil {
-		log.Fatalf("register HealthService: %v", err)
-	}
-
 	if err := authv1.RegisterAuthServiceHandlerFromEndpoint(
 		ctx, grpcMux, cfg.ServiceAddr("auth"), dialOpts,
 	); err != nil {
 		log.Fatalf("register AuthService: %v", err)
 	}
 
-    if err := blogv1.RegisterBlogServiceHandlerFromEndpoint(
-           ctx, grpcMux, cfg.ServiceAddr("blog"), dialOpts,
-        ); err != nil {
-           log.Fatalf("register BlogService: %v", err)
-    }
+	if err := blogv1.RegisterBlogServiceHandlerFromEndpoint(
+		ctx, grpcMux, cfg.ServiceAddr("blog"), dialOpts,
+	); err != nil {
+		log.Fatalf("register BlogService: %v", err)
+	}
 
 	if err := stakeholdersv1.RegisterStakeholderServiceHandlerFromEndpoint(
 		ctx, grpcMux, cfg.ServiceAddr("stakeholders"), dialOpts,
